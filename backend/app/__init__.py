353c49f82b7d83323d36acedbb309c5dae18aba4
fazal <fazalktk93@gmail.com>
2025-09-01 22:43:11 +0500

    fdf

--- backend/app/__init__.py
+++
@@ -1,6 +0,0 @@
-# backend/app/__init__.py
-# Make "from app import schemas, models" work.
-from . import schemas as schemas  # noqa: F401
-from . import models as models    # noqa: F401
-
-__all__ = ["schemas", "models"]
