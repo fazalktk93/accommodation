bff3523dfa3f68178171ee81e2eb64b9e34d3244
fazal <fazalktk93@gmail.com>
2025-09-03 08:47:19 +0500

    fdsf

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,10 @@
 # backend/app/core/config.py
 from pydantic import BaseSettings
-from typing import List
+from typing import List, Optional
 
 class Settings(BaseSettings):
     ENV: str = "development"
+    BACKEND_CORS_ORIGIN_REGEX: Optional[str] = r"^https?://(localhost|127\.0\.0\.1|10\.\d+\.\d+\.\d+|192\.168\.\d+\.\d+|172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)(:\d+)?$"
     SQLALCHEMY_DATABASE_URL: str = "sqlite:///./accommodation.db"
     BACKEND_CORS_ORIGINS: List[str] = [
         "http://localhost:5173",
