317608551244a1598c5154649698910cc1d91bc2
fazal <fazalktk93@gmail.com>
2025-09-03 22:52:00 +0500

    fds

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,15 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HouseDetail from './pages/HouseDetail'
 import AllotmentEdit from './pages/AllotmentEdit'
-// import other pages as your app needs (Houses list, etc.)
+import HouseAllotmentHistory from './pages/HouseAllotmentHistory' // ✅ add this
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* your existing routes */}
-        <Route path="/houses/:id/allotments" element={<HouseDetail />} />
+        {/* House details page (keep if you use it elsewhere) */}
+        <Route path="/houses/:id" element={<HouseDetail />} />
+
+        {/* ✅ history should render HouseAllotmentHistory */}
+        <Route path="/houses/:houseId/allotments" element={<HouseAllotmentHistory />} />
+        {/* ✅ legacy alias (if somewhere still links with :id) */}
+        <Route path="/houses/:id/allotments" element={<HouseAllotmentHistory />} />
+
+        {/* edit allotment */}
         <Route path="/allotments/:id/edit" element={<AllotmentEdit />} />
+
         {/* add a fallback as needed */}
       </Routes>
     </BrowserRouter>
